b72213e6154d772cac4de78934873b59680e6b6b
Warit Santaputra <waritsan@gmail.com>
2019-04-27 11:15:59 +0700

    List the matched items

--- index.js
+++ index.js
@@ -2,16 +2,14 @@ const request = require('request')
 const cheerio = require('cheerio')
 const fs = require('fs')
 
-const flashSaleUri = 'https://shopee.co.th/api/v2/flash_sale/get_items?promotionid=2000017717'
+const flashSaleUri = 'https://shopee.co.th/api/v2/flash_sale/get_items'
 
 request.get(flashSaleUri, (err, _, body) => {
     if (err) return console.log(err)
     writeFile(body)
-    const items = JSON.parse(body).data.items;
-    items.forEach(item => {
-        console.log(item.name)
-    });
-
+    const items = JSON.parse(body).data.items
+    const matchItems = items.filter(item => item.name.includes('Toshiba'))
+    console.log(matchItems)
 })
 
 function writeFile(data) {

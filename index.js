7e4df81e18679110039da338839bf0b904c84e6c
Warit Santaputra <waritsan@gmail.com>
2019-04-28 13:34:18 +0700

    Add array of myItems

--- index.js
+++ index.js
@@ -2,14 +2,19 @@ const request = require('request')
 const cheerio = require('cheerio')
 const fs = require('fs')
 
-const flashSaleUri = 'https://shopee.co.th/api/v2/flash_sale/get_items'
+const flashSaleUri = 'https://shopee.co.th/api/v2/flash_sale/get_items?promotionid=2000017728'
+const myItemNames = ['Seagate', 'Cherilon']
 
 request.get(flashSaleUri, (err, _, body) => {
     if (err) return console.log(err)
     writeFile(body)
-    const items = JSON.parse(body).data.items
-    const matchItems = items.filter(item => item.name.includes('Toshiba'))
-    console.log(matchItems)
+    const flashSaleItems = JSON.parse(body).data.items
+    myItemNames.forEach(myItemName => {
+        const matchItems = flashSaleItems.filter(flashSaleItem => flashSaleItem.name.includes(myItemName))
+        if (matchItems.length > 0) {
+            console.log(matchItems)
+        }
+    })
 })
 
 function writeFile(data) {
@@ -17,3 +22,4 @@ function writeFile(data) {
         if (err) console.log(err)
     })
 }
+

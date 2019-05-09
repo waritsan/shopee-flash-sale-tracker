a4d9fd885a4fb00943a9e0270fb864cc7cac727e
Warit Santaputra <waritsan@gmail.com>
2019-05-09 23:25:42 +0700

    Fix reference error.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const itemRouter = require('./routes/itemRouter')
+const preferencesRouter = require('./routes/preferencesRouter')
 
 const app = express()
 const port  = process.env.PORT || 8080
